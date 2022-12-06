e9ab144f80aa2fa034642f5707b612419a38f419
prashant pokhriyal <ppokhriyal4@gmail.com>
2022-12-06 14:57:03 +0530

    updated install script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 sudo sleep 30
 sudo apt-get update
 sudo apt-get install -y apache2
-sudo chmod -R /var/www/html/index.html
+sudo chmod -R 777 /var/www/html/index.html
 sudo echo "<h1>Welcome to Packer $(hostname -f)</h1>" > /var/www/html/index.html
